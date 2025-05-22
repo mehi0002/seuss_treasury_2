17a7cb11234d67ba7533b3d3644d0858a817a7a8
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2025-05-22 15:01:54 -0400

    Switching to hash router so it will work with github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
 import Bookshelf from './routes/Books.jsx';
@@ -9,7 +9,7 @@ import Quotes from './routes/Quotes.jsx';
 
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
